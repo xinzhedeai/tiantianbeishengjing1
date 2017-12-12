b12eb3f466a2c13662abe191928849bcadee5753
xinzhedeai <2291006094@qq.com>
2017-12-12 21:13:10 +0800

    junit test class

---
+++ tiantainbeishengjing/src/test/java/tiantainbeishengjing/DateAddDateTest.java
@@ -0,0 +1,28 @@
+package tiantainbeishengjing;
+
+import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Test;
+
+public class DateAddDateTest {
+
+	@Test
+	public void test() throws ParseException {
+         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+         Date currdate;
+         currdate = format.parse("2017-11-2");
+         System.out.println("现在的日期是：" + currdate);
+         Calendar ca = Calendar.getInstance();
+         ca.setTime(currdate);
+         ca.add(Calendar.DATE, 1);// num为增加的天数，可以改变的
+         currdate = ca.getTime();
+         String enddate = format.format(currdate);
+         System.out.println("增加天数以后的日期：" + enddate);
+	}
+
+}
